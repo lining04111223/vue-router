3dd6689cffedeed2dbe60898e494c8d5b25c5fdd
Ning Li <lining04111223@gmail.com>
2023-01-10 20:25:36 -0800

    create router

---
+++ src/router/index.js
@@ -0,0 +1,29 @@
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import AboutView from "../views/AboutView.vue";
+import JobView from "../views/JobView.vue";
+
+const routes = [
+  {
+    path: "/",
+    name: "home",
+    component: HomeView,
+  },
+  {
+    path: "/about",
+    name: "about",
+    component: AboutView,
+  },
+  {
+    path: "/jobs",
+    name: "jobs",
+    component: JobView,
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
+
+export default router;
---
+++ src/views/JobView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>Jobs</h1>
+    <div v-for="job in jobs" :key="job.id">
+      {{ job.title }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      jobs: [
+        { title: "UX designer", id: 1 },
+        { title: "web developer", id: 2 },
+        { title: "vue developer", id: 3 },
+      ],
+    };
+  },
+};
+</script>
+
+<style></style>
